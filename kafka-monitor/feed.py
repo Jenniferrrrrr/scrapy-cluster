8ce0f6019158c437e16221795af4a08de1b19ba0
bradafzali <brad.afzali@gmail.com>
2018-04-05 14:27:38 -0700

    Adding micro sample csv and deleting Consumer class from feed.py

--- kafka-monitor/feed.py
+++ kafka-monitor/feed.py
@@ -3,7 +3,7 @@ import threading, logging, time
 import multiprocessing
 import csv
 
-from kafka import KafkaConsumer, KafkaProducer
+from kafka import KafkaProducer
 
 
 class Producer(threading.Thread):
@@ -28,34 +28,10 @@ class Producer(threading.Thread):
                 time.sleep(0.1)
         producer.close()
 
-class Consumer(multiprocessing.Process):
-    def __init__(self):
-        multiprocessing.Process.__init__(self)
-        self.stop_event = multiprocessing.Event()
-        
-    def stop(self):
-        self.stop_event.set()
-        
-    def run(self):
-        consumer = KafkaConsumer(bootstrap_servers='kafka:9092',
-                                 auto_offset_reset='earliest',
-                                 consumer_timeout_ms=1000)
-        consumer.subscribe(['demo.incoming'])
 
-        while not self.stop_event.is_set():
-            for message in consumer:
-                print(message)
-        break
-                if self.stop_event.is_set():
-                    break
-
-        consumer.close()
-        
-        
 def main():
     tasks = [
         Producer()
-        # Consumer()
     ]
 
     for t in tasks:
