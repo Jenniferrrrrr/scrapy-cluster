fbd7268b777259d91f2f192039d32acf9a3f98cc
bradafzali <brad.afzali@gmail.com>
2018-05-03 18:07:35 -0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,11 @@
+# Kill and remove previous docker containers
 docker-compose down
+# Delete previous scrape outputs
 sudo rm -f /vol_b/data/scrapy_cluster_data/data.db
+
+# Build containers
 docker-compose build
+# Start up new containers
 docker-compose up -d
+# Scale up the crawler container to have 10 total copies
 docker-compose scale crawler=10
